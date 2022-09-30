3137458752a5cd2c7651c04572dae0019d565b99
Huxley Marvit <huxmarv@nuevaschool.org>
2022-09-30 12:33:47 -0700

    general setup tsx, react, tailwind

---
+++ src/web/src/pages/App.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import '../../styles/App.css';
+
+
+export default function App() {
+  return (
+	<div className="App test">
+	    uwu
+	    <div className="test">owo</div>
+	</div>
+  );
+}
