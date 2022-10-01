6f7d6fe055c518748fad9ba76131c0c98ccdab5d
Huxley Marvit <huxmarv@nuevaschool.org>
2022-10-01 22:34:12 +0000

    NW: nested movable elements

--- src/web/src/pages/Board.tsx
+++ src/web/src/pages/Board.tsx
@@ -1,37 +1,15 @@
 import React, {} from 'react';
 import '../../styles/App.css';
 import InfiniteViewer from "react-infinite-viewer";
-
-//import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
-
-//export default function Board(props: any) {
-//    return (
-//        <div className="h-screen border-2 border-blue-400">
-//            <TransformWrapper 
-//                className="border-purple-400 border-3"
-//                limitToBounds={false}
-//            >
-//                <TransformComponent>
-//                    <p className="w-screen h-screen border-2 border-green-400"> 
-//                        yup
-//                    </p>
-//                </TransformComponent>
-//            </TransformWrapper>
-//        </div>
-//    );
-//}
+import DraggableTest from "../components/DraggableTest";
 
 export default function Board(props: any) {
     return (
 	<div className="">
 	    <InfiniteViewer
 		className="h-screen bg-gray-500 viewer"
-		//margin={0}
-		//threshold={0}
-		//rangeX={[0, 0]}
-		//rangeY={[0, 0]}
 		onScroll={e => {
-		    console.log(e);
+		    //console.log(e);
 		}}
 		//usePinch={true}
 		useMouseDrag={true}
@@ -40,11 +18,10 @@ export default function Board(props: any) {
 		displayVerticalScroll={false}
 		displayHorizontalScroll={false}
 	    >
-		<div className="border-2 border-green-400 bg-slate-400 viewport">
-		   woahhhhhhhh 
-		    <div className="bg-red-600"> hiiiiiiiiiiiiiii </div>
+		<div>
+		    <DraggableTest />
+		    <div className="w-20 h-20 bg-red-500">  yeahhh </div>
 		</div>
-		    <div className="bg-red-600"> hrrrrrrrr </div>
 	    </InfiniteViewer>
 	    </div>
     );
