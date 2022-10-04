0b0d347e563fb2d6a69c515f6916f55ebf06f317
Huxley Marvit <huxmarv@nuevaschool.org>
2022-10-03 21:33:33 -0700

    start frame movement tech

--- src/web/src/components/DraggableTest.tsx
+++ src/web/src/components/DraggableTest.tsx
@@ -5,9 +5,21 @@ export default function DraggableTest(props: any) {
 
 
     const targetRef = React.useRef<HTMLDivElement>(null);
-    //const moveableRef = React.useRef<Moveable>(null);
+    const movRef = React.useRef<Moveable>(null);
 
-    
+    //const moveableRef = React.useRef<Moveable>(null);
+    //
+    React.useEffect(() => {
+	setTimeout(() => {
+	    moveToCoords(props.x, props.y)
+	}, 10)
+    }, [])
+
+    const moveToCoords = (x: number, y: number) => {
+	console.log("moving", x, y)
+	movRef.current.moveable.request("draggable", { x: x, x: x }, true);
+    }
+
     return (
 	<>
 	    <div ref={targetRef} className="" style={{width: "min-content"}}>
@@ -17,6 +29,14 @@ export default function DraggableTest(props: any) {
 	    target={targetRef}
 	    container={null}
 	    origin={false}
+	    ref={movRef}
+	    onClick={(e) => {
+		console.log(movRef.current, e)
+		//console.log(movRef.current.moveable.request)
+
+		movRef.current.moveable.request("draggable", { x: 420, y: 69 }, true);
+		//movRef.current.moveable.request("draggable", { deltaX: 10, deltaY: 10 });
+	    }}
 
 	    /* Resize event edges */
 	    edge={true}
--- src/web/src/pages/Board.tsx
+++ src/web/src/pages/Board.tsx
@@ -6,14 +6,21 @@ import "../../styles/Frame.css";
 
 export default function Board(props: any) {
 
+    const vref = React.useRef<InfiniteViewer>(null);
+
     const [frames, addFrame] = React.useState(
 	[
+	    //<div>ss </div>,
 	    <DraggableTest
+		x={0}
+		y={0}
 		content={
 		    <div className="frame"> t2asjdflkasd </div>
 		}
 	    />,
 	    <DraggableTest
+		x={0}
+		y={0}
 		content={
 		    <div className="frame"> laksdfffffffffffffffffffff; </div>
 		}
@@ -21,18 +28,45 @@ export default function Board(props: any) {
 	]
     );
 
+    //const handleClick = (e: any) => {
+    //    if (e.detail === 2) {
+    //        //console.log("Double click", e.clientX, e.clientY);
+    //        console.log(e, e.target.offsetLeft, e.target.offsetLeft-e.clientX)
+    //        //const mouseX = e.pageX - div.offsetLeft;
+    //        //const mouseY = e.pageY - div.offsetTop;
+    //        testAddFrame();
+    //        console.log(vref.current.getScrollLeft(false))
+    //        console.log(vref.current.getRangeY(false, false))
+    //    }
+    //}
+
     const handleClick = (e: any) => {
-	if (e.detail === 2) {
-	    //console.log("Double click", e.clientX, e.clientY);
-	    console.log(e, e.target.offsetLeft, e.target.offsetLeft-e.clientX)
-	    //const mouseX = e.pageX - div.offsetLeft;
-	    //const mouseY = e.pageY - div.offsetTop;
-	    testAddFrame();
-	}
+	//e.clientX
+	//e.clientY
+
+	const [absx, absy] = getPlacePosition(e.inputEvent.layerX, e.inputEvent.layerY)
+
+	testAddFrame(absx, absy)
+	//console.log(e, "here")
     }
 
-    const testAddFrame = () => {
+    const getPlacePosition = (relx: number, rely: number) => {
+	const x = vref.current.getScrollLeft(false);
+	const y = vref.current.getScrollTop(false);
+
+	//console.log(relx+x, rely+y, "hi")
+	//const absx = relx+x;
+	//const absy = rely+y;
+	const absx = relx;
+	const absy = rely;
+	return [absx, absy]
+	//console.log(x +
+    }
+
+    const testAddFrame = (x: number, y: number) => {
 	const tempFrame = <DraggableTest
+		x={x}
+		y={y}
 		content={
 		    <div className="frame"> {Math.random()} </div>
 		}
@@ -42,22 +76,28 @@ export default function Board(props: any) {
 
     return (
 	<div className=""
-	    onClick={handleClick}
+	    //onClick={handleClick}
 	>
 	    <InfiniteViewer
 		className="h-screen bg-gray-500 viewer"
 		onScroll={e => {
-		    //console.log(e);
+		    //console.log(vref.current.infiniteViewer.scrollLeft, vref.current.infiniteViewer.scrollTop)
+		    console.log(vref.current.getScrollLeft(false), vref.current.getScrollTop(false))
+		    //console.log(vref.current.getRangeY(false, false))
 		}}
+
+		onClick={handleClick}
 		//usePinch={true}
 		useMouseDrag={true}
 		useAutoZoom={true}
+		ref={vref}
 		//wheelScale={0.001}
 		//displayVerticalScroll={false}
 		//displayHorizontalScroll={false}
 
 		onDragStart={e => {
-		    console.log(e, e.inputEvent.layerX, e.inputEvent.layerY)
+		    //console.log(e, e.inputEvent.layerX, e.inputEvent.layerY)
+		    handleClick(e)
 		}}
 	    >
 		<div>
