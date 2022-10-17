0b75b976c802714566536dfa9a75802c05805bcd
Nicholas Assaderaghi <nicassa@nuevaschool.org>
2022-10-17 11:31:33 -0700

    Cleaned up sidebar code.

--- src/web/src/pages/AppView.tsx
+++ src/web/src/pages/AppView.tsx
@@ -2,37 +2,19 @@ import * as React from 'react';
 import '../../styles/App.css';
 import "../../styles/Background.css"
 import { ProSidebarProvider, useProSidebar } from 'react-pro-sidebar';
-import { Sidebar, Menu, MenuItem, SubMenu } from 'react-pro-sidebar';
-import{ DetailsMore } from 'css.gg/icons/tsx/DetailsMore';
-import { PushChevronRight } from 'css.gg/icons/tsx/PushChevronRight';
-import { PushChevronLeft } from 'css.gg/icons/tsx/PushChevronLeft';
+import { OurSidebar } from './Sidebar';
 
 import { BoardView } from './BoardView';
 import { EntityViewProps } from '../util/hooks';
 
-function WithinBackground({ target }: EntityViewProps) {
-	const { collapseSidebar } = useProSidebar();
-	return (<>
-		<Sidebar>
-			<Menu>
-				<MenuItem onClick={() => collapseSidebar()}><PushChevronRight></PushChevronRight></MenuItem>
-				<SubMenu icon={<DetailsMore></DetailsMore>}>
-					<MenuItem> Pie charts </MenuItem>
-					<MenuItem> Line charts </MenuItem>
-				</SubMenu>
-				<MenuItem> Calendar </MenuItem>
-			</Menu>
-		</Sidebar>
-		<div className="board_outer">
-			<BoardView target={target} />
-		</div>
-	</>);
-}
 export default function AppView({ target }: EntityViewProps) {
 	return (
 		<ProSidebarProvider>
 			<div className="bg-matcha-normal the_background">
-				<WithinBackground target={target}></WithinBackground>
+			<OurSidebar></OurSidebar>
+			<div className="board_outer">
+				<BoardView target={target} />
+			</div>
 			</div>
 		</ProSidebarProvider>);
 }
--- src/web/src/pages/Sidebar.tsx
+++ src/web/src/pages/Sidebar.tsx
@@ -1,5 +1,26 @@
 import * as React from "react";
+import { ProSidebarProvider, useProSidebar } from 'react-pro-sidebar';
+import { Sidebar, Menu, MenuItem, SubMenu } from 'react-pro-sidebar';
+import{ DetailsMore } from 'css.gg/icons/tsx/DetailsMore';
+import { PushChevronRight } from 'css.gg/icons/tsx/PushChevronRight';
+import { PushChevronLeft } from 'css.gg/icons/tsx/PushChevronLeft';
 
-export function Sidebar() {
-    return <></>;
+export function OurSidebar() {
+    const { collapseSidebar } = useProSidebar();
+	const [expanded, setExpanded] = React.useState(false);
+    
+    return (<>
+        <Sidebar defaultCollapsed={true}>
+            <Menu>
+                <MenuItem onClick={() => {setExpanded(!expanded); collapseSidebar();}}>
+                    {expanded ? <PushChevronLeft /> : <PushChevronRight />}
+                </MenuItem>
+                <SubMenu icon={<DetailsMore></DetailsMore>}>
+                    <MenuItem> Pie charts </MenuItem>
+                    <MenuItem> Line charts </MenuItem>
+                </SubMenu>
+                <MenuItem> Calendar </MenuItem>
+            </Menu>
+        </Sidebar>
+    </>);
 }
