1af78c87734dff4b88e3ca8c30fe835114be6ed0
Nicholas Assaderaghi <nicassa@nuevaschool.org>
2022-10-17 11:04:42 -0700

    We somewhat have a sidebar now!

--- src/web/src/pages/AppView.tsx
+++ src/web/src/pages/AppView.tsx
@@ -4,16 +4,38 @@ import * as React from 'react';
 import '../../styles/App.css';
 import "../../styles/Board.css"
 import "../../styles/Background.css"
-import { DetailsMore } from "../../../../node_modules/css.gg/icons/tsx/DetailsMore.js";
-
+import { ProSidebarProvider, useProSidebar } from 'react-pro-sidebar';
+import { Sidebar, Menu, MenuItem, SubMenu } from 'react-pro-sidebar';
+import{ DetailsMore } from 'css.gg/icons/tsx/DetailsMore';
+import { PushChevronRight } from 'css.gg/icons/tsx/PushChevronRight';
+import { PushChevronLeft } from 'css.gg/icons/tsx/PushChevronLeft';
 
 import { BoardView } from './BoardView';
 import { EntityViewProps } from '../util/hooks';
 
-export default function AppView({ target }: EntityViewProps) {
-	return <div className="bg-matcha-normal the_background">
+function WithinBackground({ target }: EntityViewProps) {
+	const { collapseSidebar } = useProSidebar();
+	return (<>
+		<Sidebar>
+			<Menu>
+				<MenuItem onClick={() => collapseSidebar()}><PushChevronRight></PushChevronRight></MenuItem>
+				<SubMenu icon={<DetailsMore></DetailsMore>}>
+					<MenuItem> Pie charts </MenuItem>
+					<MenuItem> Line charts </MenuItem>
+				</SubMenu>
+				<MenuItem> Calendar </MenuItem>
+			</Menu>
+		</Sidebar>
 		<div className="board_outer">
 			<BoardView target={target} />
 		</div>
-	</div>;
+	</>);
+}
+export default function AppView({ target }: EntityViewProps) {
+	return (
+		<ProSidebarProvider>
+			<div className="bg-matcha-normal the_background">
+				<WithinBackground target={target}></WithinBackground>
+			</div>
+		</ProSidebarProvider>);
 }
