a674af7768dcd7ca952480dc7e5a5be1ef14e384
Nicholas Assaderaghi <nicassa@nuevaschool.org>
2022-10-19 19:29:37 -0700

    Polished up sidebar.
    Also added comments and ascii art (shh).

--- src/web/src/pages/AppView.tsx
+++ src/web/src/pages/AppView.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+
 import '../../styles/App.css';
 import "../../styles/Background.css"
 import { OurSidebar } from './Sidebar';
@@ -11,10 +12,13 @@ export default function AppView({ target }: EntityViewProps) {
 	return (
 		<ProSidebarProvider>
 			<div className="bg-matcha-normal the_background">
-			<OurSidebar></OurSidebar>
-			<div className="board_outer">
-				<BoardView target={target} />
-			</div>
+				{/** Called "OurSidebar" because the lib we use for the sidebar calls their class "Sidebar". */}
+				<OurSidebar></OurSidebar> 
+				{/** We have two css classes, board_outer and board_inner because the css with the infinite canvas behaves funky. */}
+				<div className="board_outer"> 
+					{/** Where all the magic occurs ;) */}
+					<BoardView target={target} />
+				</div>
 			</div>
 		</ProSidebarProvider>);
 }
--- src/web/src/pages/Sidebar.tsx
+++ src/web/src/pages/Sidebar.tsx
@@ -1,26 +1,186 @@
+// TODO: add more buttons with more functionality 
+// TODO: figure out how to color icons
+// TODO: figure out how to get rid of the annoying 1 px border
+
 import * as React from "react";
-import { ProSidebarProvider, useProSidebar } from 'react-pro-sidebar';
-import { Sidebar, Menu, MenuItem, SubMenu } from 'react-pro-sidebar';
-import{ DetailsMore } from 'css.gg/icons/tsx/DetailsMore';
+
+import { Sidebar, Menu, MenuItem, useProSidebar/*, SubMenu*/ } from 'react-pro-sidebar';
+import { SoftwareDownload } from 'css.gg/icons/tsx/SoftwareDownload';
 import { PushChevronRight } from 'css.gg/icons/tsx/PushChevronRight';
 import { PushChevronLeft } from 'css.gg/icons/tsx/PushChevronLeft';
+import { FormatText } from 'css.gg/icons/tsx/FormatText';
+import { Infinity } from 'css.gg/icons/tsx/Infinity';
+
+// import { DetailsMore } from 'css.gg/icons/tsx/DetailsMore';
+
 
 export function OurSidebar() {
-    const { collapseSidebar } = useProSidebar();
-	const [expanded, setExpanded] = React.useState(false);
-    
+    const { collapseSidebar } = useProSidebar(); // allows us to expand/minimize sidebar
+    const [expanded, setExpanded] = React.useState(false); // is the sidebar expanded or minimized
+    const [isModeMath, setMode] = React.useState(false); // is the default mode for blocks 
+
     return (<>
-        <Sidebar defaultCollapsed={true}>
+        {/** All of these attributes of sidebar are self-explanatory, except that trainsitionDuration is # of ms to animate collapse/expand. */}
+        <Sidebar defaultCollapsed={true} backgroundColor="rgb(139 168 136)" transitionDuration={250} collapsedWidth="80px" width="228px">
             <Menu>
-                <MenuItem onClick={() => {setExpanded(!expanded); collapseSidebar();}}>
-                    {expanded ? <PushChevronLeft /> : <PushChevronRight />}
+                <MenuItem icon={expanded ? <PushChevronLeft /> : <PushChevronRight />} onClick={() => { setExpanded(!expanded); collapseSidebar(); }}>
+                    Collapse
                 </MenuItem>
-                <SubMenu icon={<DetailsMore></DetailsMore>}>
-                    <MenuItem> Pie charts </MenuItem>
-                    <MenuItem> Line charts </MenuItem>
-                </SubMenu>
-                <MenuItem> Calendar </MenuItem>
+                <MenuItem icon={<SoftwareDownload />}> Save </MenuItem>
+                <MenuItem icon={isModeMath ? <Infinity /> : <FormatText />} onClick={() => { setMode(!isModeMath) }}>
+                    Default Block Mode
+                </MenuItem>
+                {
+                    /*
+                    This is just to show how you would do a submenu.
+    
+                    <SubMenu icon={<DetailsMore></DetailsMore>} label="Visual">
+                        <MenuItem> Graph </MenuItem>
+                        <MenuItem> Table </MenuItem>
+                    </SubMenu>
+                    */
+                }
             </Menu>
         </Sidebar>
     </>);
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⡄⣬⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠗⣸⠷⣙⣭⣭⣭⣭⣍⣛⣯⠍⣰⣿⣿⣿
+⣿⠉⠀⠙⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟⣩⣭⠭⡭⢉⣙⠛⠿⢿⣿⣟⢱⣾⣥⣾⣿⣿⣿⣿⣿⣛⣛⠡⣾⣿⣿⣿⣿
+⣿⠆⠊⠉⠀⠶⣯⡹⣿⣿⣿⣿⣿⣿⣿⣿⠛⠿⣿⣿⠿⣃⣴⡾⣢⣶⣿⣿⣇⠙⠷⢶⣭⣀⠀⠀⢉⣛⠛⠿⣿⣿⣿⣿⣿⣮⠻⣿⣿⣿
+⢇⣾⠀⢀⣠⣴⣆⢳⡜⢿⣿⣿⣿⣿⣿⣿⣧⡻⣶⡶⢾⡿⢫⣾⣿⣿⣿⢟⡍⠈⣿⣦⣄⠙⠳⣄⠘⣿⣿⣿⣶⣝⢿⣿⣿⣿⣷⡹⣿⣿
+⡘⣏⢰⣿⣿⣿⣿⣧⠻⣌⢿⣿⣿⣿⣿⣿⣿⣿⡌⣵⣿⣷⣿⣿⣿⡿⡣⢋⣤⡄⢹⣿⣿⣷⡤⠙⠷⠈⢿⣿⣿⣿⣧⣻⣿⠿⣿⣧⢻⣿
+⣿⡜⢧⠻⣿⣿⣿⣿⣧⠹⣦⢻⣿⣿⣿⣿⣿⡿⢸⠿⣫⣿⣿⡿⠋⣠⣴⣿⣿⡇⢸⣿⣿⢻⠇⡄⣰⣶⠈⢿⣿⣿⣿⣿⣿⢸⣦⣝⠸⢿
+⣿⣿⣌⢷⡙⠟⢉⣤⣴⣦⡸⣧⠹⣿⣿⣍⣉⠡⠶⠿⣛⣋⠁⣰⣿⣿⣿⣛⣛⡇⠸⣿⣿⡜⠆⠁⠙⠉⢸⡌⢿⣿⣿⣿⣿⢸⡿⢫⡄⠿
+⣿⣿⣿⣦⠻⣄⠋⠿⠟⠉⢡⣶⣦⣾⣿⣿⣿⢣⢸⣿⣿⠇⠚⢿⣿⣿⡻⠿⣿⣷⡐⢺⣿⣿⡄⠀⠀⠀⣼⡇⡌⣿⣿⣿⣿⢀⣴⣿⠀⠀
+⣿⣿⣿⣿⣷⡙⢸⠰⠶⠲⠶⠦⠹⣿⣿⠿⢃⣛⡁⣿⣿⢰⡄⠀⣿⣿⣶⠀⣬⡙⠇⢰⣍⡛⠷⣠⡄⢠⡿⢱⡟⠸⣿⣿⡟⣼⠋⠁⠀⣴
+⣿⣿⣿⣿⣿⣿⣤⣾⣿⣷⡀⠀⠀⡙⣿⣿⡟⢻⣿⡜⣿⢸⢣⣤⣿⣿⣿⣤⣼⣷⣿⣌⠋⠉⠀⡅⠇⠸⠣⠟⣴⠃⣿⡿⢱⣿⣿⢠⣼⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡄⠀⢀⡌⠟⢀⣾⣿⣿⣮⡀⠸⣿⣿⣿⣿⣿⣿⣿⣿⠏⠀⠸⠼⣣⣴⣴⣶⣿⠏⡀⠟⣡⣻⣿⡇⠈⠉⠉
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡄⠟⠁⢀⣾⡿⠿⠟⣿⣿⣦⠹⣿⣿⣿⣿⣿⡿⠁⠀⠠⣶⣾⣿⣿⣿⣿⡏⣼⠇⣼⡿⠙⠟⠀⢠⣶⣶
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⡄⠀⠀⠀⠀⣤⣶⣿⣿⣟⣤⣹⠟⡛⠋⠁⠀⠀⠀⠀⠹⣿⣿⣿⡿⣫⣾⡿⡘⠋⠀⠈⠉⠛⠓⠾⢽
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡁⠀⠀⠀⠀⢸⣿⣿⣿⣿⢹⣿⡄⠁⠀⠀⠀⠀⠀⠀⠀⠙⠟⢩⣶⣿⣿⣿⡇⠀⠀⢀⣀⣀⣀⠀⠀
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⡀⠀⠀⠀⠹⣿⣿⠟⢋⠈⠉⠂⠀⠀⠀⠀⠀⢀⣠⣴⣾⣿⣶⡮⠍⠛⠋⠑⢿⣦⣝⡻⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⡀⠀⠀⠀⠘⠿⢸⣿⣭⣤⡀⠀⠀⠀⣠⠶⣿⣿⣿⣿⣿⠟⣁⢾⣿⡆⠀⠀⠉⢿⣿⣮⣝⠿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⠀⠀⠀⠀⠀⢨⣿⣿⣿⡇⣠⣶⣿⣿⣶⣮⣛⣟⡛⠁⠀⢻⡇⢻⡇⣾⣷⣦⣌⡻⢿⣿⣿⣮
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⢠⡄⢀⠀⠀⢻⡿⢫⣾⣿⣿⣿⣿⣿⣿⣿⣿⠁⠀⣴⣦⠳⣜⡇⣿⣿⣿⣿⣿⣷⣬⡛⢿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⢸⠇⣿⣷⡀⠀⠰⣿⣿⣿⣿⣿⣿⣿⣿⣿⠇⠀⠀⠿⣣⢰⣮⡃⣿⣿⣿⣿⣿⣿⣿⣿⣶
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⠈⠀⣿⣿⣷⡀⠀⠙⣿⣿⣿⣿⣿⣿⣿⡟⠀⠀⠀⢰⡟⣼⣿⡇⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⢀⣼⠀⠙⣿⣿⣿⡄⠀⠈⠿⠛⢿⣿⣿⣿⣿⡦⠀⢀⡄⣴⡌⣿⡇⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⢃⣾⡟⣠⠀⣿⣿⣿⣷⠂⠀⠀⠀⠀⠹⣿⣿⠏⠀⢀⣾⣇⠛⣀⢿⣇⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣷⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⢸⠋⣴⣿⠀⢹⣿⣿⢾⣆⠀⠀⠀⠀⠀⠘⠃⠀⢀⣾⣿⣿⠈⠃⣼⣿⢸⣿⣿⣿⣿⣿⣿⣿⣿
+⣀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⣾⣿⠟⢠⣆⢻⣿⣾⣿⣷⡀⠀⠀⠀⠀⠀⠠⣭⣍⠩⣭⣴⣤⡉⠭⢸⣿⣿⣿⣿⣿⣿⣿⣿
+
+quu..__
+ $$$b  `---.__
+  "$$b        `--.                          ___.---uuudP
+   `$$b           `.__.------.__     __.---'      $$$$"              .
+     "$b          -'            `-.-'            $$$"              .'|
+       ".                                       d$"             _.'  |
+         `.   /                              ..."             .'     |
+           `./                           ..::-'            _.'       |
+            /                         .:::-'            .-'         .'
+           :                          ::''\          _.'            |
+          .' .-.             .-.           `.      .'               |
+          : /'$$|           .@"$\           `.   .'              _.-'
+         .'|$u$$|          |$$,$$|           |  <            _.-'
+         | `:$$:'          :$$$$$:           `.  `.       .-'
+         :                  `"--'             |    `-.     \
+        :##.       ==             .###.       `.      `.    `\
+        |##:                      :###:        |        >     >
+        |#'     `..'`..'          `###'        x:      /     /
+         \                                   xXX|     /    ./
+          \                                xXXX'|    /   ./
+          /`-.                                  `.  /   /
+         :    `-  ...........,                   | /  .'
+         |         ``:::::::'       .            |<    `.
+         |             ```          |           x| \ `.:``.
+         |                         .'    /'   xXX|  `:`M`M':.
+         |    |                    ;    /:' xXXX'|  -'MMMMM:'
+         `.  .'                   :    /:'       |-'MMMM.-'
+          |  |                   .'   /'        .'MMM.-'
+          `'`'                   :  ,'          |MMM<
+            |                     `'            |tbap\
+             \                                  :MM.-'
+              \                 |              .''
+               \.               `.            /
+                /     .:::::::.. :           /
+               |     .:::::::::::`.         /
+               |   .:::------------\       /
+              /   .''               >::'  /
+              `',:                 :    .'
+                                   `:.:'
+*/
