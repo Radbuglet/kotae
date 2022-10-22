535cf7f7285deb625c5ae5db66e59c60b65d5ed4
Nicholas Assaderaghi <nicassa@nuevaschool.org>
2022-10-21 20:22:17 -0700

    Comprehensively commented sidebar code.
    Thank you Huxley for all you did !

--- src/web/src/pages/Sidebar.tsx
+++ src/web/src/pages/Sidebar.tsx
@@ -1,44 +1,44 @@
 import * as React from "react";
 
-import { DEFAULT_INSERTION_MODE, SELECT_ACTIVE, RESET_MY_ZOOM } from "../blocks/factory";
-import { Sidebar, Menu, MenuItem, useProSidebar/*, SubMenu*/ } from 'react-pro-sidebar';
-import { EntityViewProps, useListenable, wrapWeakReceiver } from "../util/hooks";
+import { DEFAULT_INSERTION_MODE, SELECT_ACTIVE, RESET_MY_ZOOM } from "../blocks/factory"; // data we're accessing from IR
+import { EntityViewProps, useListenable, wrapWeakReceiver } from "../util/hooks"; // IR utilities
+import { Sidebar, Menu, MenuItem, useProSidebar } from 'react-pro-sidebar'; // sidebar library
 
-import { FaAngleDoubleRight, FaAngleDoubleLeft, FaInfinity, FaTextHeight, FaGamepad, FaDownload } from 'react-icons/fa';
-import { TbMathAvg } from 'react-icons/tb';
-import { BiText, BiSelection } from 'react-icons/bi';
+// icons 
 import { MdOutlineShareLocation } from 'react-icons/md';
+import { BiText, BiSelection } from 'react-icons/bi';
+import { FaAngleDoubleRight } from 'react-icons/fa';
+import { TbMathAvg } from 'react-icons/tb';
 
-// import { DetailsMore } from 'css.gg/icons/tsx/DetailsMore';
 
 
 export function OurSidebar({ target }: EntityViewProps) {
     const { collapseSidebar } = useProSidebar(); // allows us to expand/minimize sidebar
     const [expanded, setExpanded] = React.useState(false); // is the sidebar expanded or minimized
 
-    const block_insertion_mode = target.deepGet(DEFAULT_INSERTION_MODE);
-    const select_toggle = target.deepGet(SELECT_ACTIVE);
-    const reset_zoom = target.deepGet(RESET_MY_ZOOM);
+    const block_insertion_mode = target.deepGet(DEFAULT_INSERTION_MODE); // access the block insertion mode (math or regular text) from the IR
+    const select_toggle = target.deepGet(SELECT_ACTIVE); // access whether or not we are toggling selecto from the IR
+    const reset_zoom = target.deepGet(RESET_MY_ZOOM); // 
 
+    // swap block insertion mode (math or regular text)
     const doCycleInsertionMode = wrapWeakReceiver(block_insertion_mode, _ => {
-        const value = (block_insertion_mode.value + 1) % 2;
+        const value = (block_insertion_mode.value + 1) % 2; // instead of being normal and using if statements, we use this to flip, from 0 -> 1 and 1 -> 0
         block_insertion_mode.value = value;
-
     });
 
+    // toggle between creating math/text block and selecto
     const doCycleSelectToggle = wrapWeakReceiver(select_toggle, _ => {
-        const value = (select_toggle.value + 1) % 2;
+        const value = (select_toggle.value + 1) % 2; // same logic as doCycleInsertionMode
         select_toggle.value = value;
-
     });
 
+    // calling this will cause zoom to reset via IR
     const doTriggerResetZoom = wrapWeakReceiver(reset_zoom, _ => {
         reset_zoom.value += 1;
-
     });
 
-    const curr_ins_mode = useListenable(block_insertion_mode);
-    const curr_select_active = useListenable(select_toggle);
+    const curr_ins_mode = useListenable(block_insertion_mode); // how we read block insertion mode 
+    const curr_select_active = useListenable(select_toggle); // how we read toggling selecto 
 
     return (<>
         {/** All of these attributes of sidebar are self-explanatory, except that trainsitionDuration is # of ms to animate collapse/expand. */}
@@ -47,7 +47,7 @@ export function OurSidebar({ target }: EntityViewProps) {
             className="sidebar" style={{ borderRight: "2px solid var(--matcha-bluish)", userSelect: "none" }}>
             <Menu
                 className="flex flex-col content-center mt-4 -ml-1"
-                renderMenuItemStyles={({ level, disabled, active }) => ({
+                renderMenuItemStyles={({ level, disabled, active }) => ({ /** how we override the sidebar's library styling */
                     '.menu-icon': {
                         //backgroundColor: 'none',
                         //backgroundColor: active? "var(--matcha-400)" : 'none',
@@ -59,7 +59,7 @@ export function OurSidebar({ target }: EntityViewProps) {
                         fontStyle: 'black',
                         borderRadius: '2px',
                         marginLeft: "-0.5px",
-                        borderLeft: active? "3px solid var(--matcha-light)": "3px solid transparent",
+                        borderLeft: active ? "3px solid var(--matcha-light)" : "3px solid transparent",
                         ':hover': {
                             borderLeft: "3px solid var(--matcha-light)",
                             paddingLeft: "5px",
@@ -75,32 +75,40 @@ export function OurSidebar({ target }: EntityViewProps) {
                     },
                 })}
             >
-                <MenuItem icon={<FaAngleDoubleRight size={25} className={`${expanded? "rotate-180" : "rotate-0"} transition duration-200`}/>} onClick={() => { setExpanded(!expanded); collapseSidebar(); }}>
+
+                {/** We use css to spin the arrow to the right direction (towards right to expand, left to collapse). */}
+                <MenuItem icon={<FaAngleDoubleRight size={25} className={`${expanded ? "rotate-180" : "rotate-0"} transition duration-200`} />} onClick={() => { setExpanded(!expanded); collapseSidebar(); }}>
                     Collapse
                 </MenuItem>
-                {/*<MenuItem icon={<SoftwareDownload />}> Save </MenuItem>*/}
-                <MenuItem icon={curr_ins_mode ? 
-                    //<p className="text-xl" style={{fontFamily: "'Inconsolata', monospace"}}>∫</p>
+
+                {
+                /** We don't have saving capability just yet ;( */
+                /** When actually implementing consider <FaDownload /> */
+                /*<MenuItem icon={<SoftwareDownload />}> Save </MenuItem>*/
+                }
+
+                {/** We toggle the icon depending on the insertion mode. */}
+                <MenuItem icon={curr_ins_mode ?
                     <TbMathAvg size={20} />
-                        
-                    : 
+                    :
                     <BiText size={25} />
-                    } onClick={doCycleInsertionMode}>
+                } onClick={doCycleInsertionMode}>
                     {curr_ins_mode ? "Text mode" : "Math mode"}
                 </MenuItem>
-                <MenuItem icon={<BiSelection size={25} /> // TODO make this sync!
-                    }
-                    active={curr_select_active!!}
+
+                {/** Active property will show an indicator for when selecto is on. */}
+                <MenuItem icon={<BiSelection size={25} />
+                }
+                    active={curr_select_active==1}
                     onClick={doCycleSelectToggle}
-                > 
+                >
                     Select mode
                 </MenuItem>
-                <MenuItem icon={<MdOutlineShareLocation size={25} />
-                    }
-                    onClick={doTriggerResetZoom}
-                > 
+
+                <MenuItem icon={<MdOutlineShareLocation size={25} />} onClick={doTriggerResetZoom}>
                     Reset zoom
                 </MenuItem>
+
                 {
                     /*
                     This is just to show how you would do a submenu.
@@ -111,12 +119,6 @@ export function OurSidebar({ target }: EntityViewProps) {
                     </SubMenu>
                     */
                 }
-                {/*<MenuItem icon={<BiSelection size={25} className="" 
-                    style={{ position: "absolute",
-                        bottom:"0"
-                    }}/> }> 
-                    Toggle Select
-                </MenuItem>*/}
             </Menu>
         </Sidebar>
     </>);
