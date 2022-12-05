ee5ed3cd319bff0ccceec151cbdc8605f6f55b61
Huxley Marvit <huxmarv@nuevaschool.org>
2022-12-05 15:50:36 -0800

    clean things

--- src/web/src/model/blocks/latex_scry.tsx
+++ src/web/src/model/blocks/latex_scry.tsx
@@ -3,72 +3,6 @@ import { Entity, Part } from "kotae-util";
 import { EntityViewProps, useListenable } from "../../util/hooks";
 import { BlockRegistry, IrBlock, ScryBlock, IrLine, IrFrame } from "kotae-common";
 import { BLOCK_FACTORY_KEY, BLOCK_KIND_INFO_KEY, BLOCK_VIEW_KEY } from "./../registry";
-//import { useSvgDrawing } from 'react-hooks-svgdrawing'
-//import { ReactSketchCanvas } from 'react-sketch-canvas';
-import { parse } from 'node-html-parser';
-
-//const useCanvas = (draw, options={}) => {
-
-//    const canvasRef = React.useRef(null)
-
-//    React.useEffect(() => {
-
-//        const canvas = canvasRef.current
-//        const context = canvas.getContext(options.context || '2d')
-//        let frameCount = 0
-//        let animationFrameId;
-//        const render = () => {
-//            frameCount++
-//                draw(context, frameCount)
-//            animationFrameId = window.requestAnimationFrame(render)
-//        }
-//        render()
-//        return () => {
-//            window.cancelAnimationFrame(animationFrameId)
-//        }
-//    }, [draw])
-//    return canvasRef
-//}
-
-//const Canvas = props => {  
-
-//    const { draw, options, ...rest } = props
-//    const { context, ...moreConfig } = options
-//    const canvasRef = useCanvas(draw, {context})
-
-//    const paintCanvas = canvasRef.current
-//    context.lineCap = 'round';
-
-//    let x = 0, y = 0;
-//    let isMouseDown = false;
-
-//    const stopDrawing = () => { isMouseDown = false; }
-//    const startDrawing = event => {
-//        isMouseDown = true;   
-//        [x, y] = [event.offsetX, event.offsetY];  
-//    }
-//    const drawLine = event => {
-//        if ( isMouseDown ) {
-//            const newX = event.offsetX;
-//            const newY = event.offsetY;
-//            context.beginPath();
-//            context.moveTo( x, y );
-//            context.lineTo( newX, newY );
-//            context.stroke();
-//            //[x, y] = [newX, newY];
-//            x = newX;
-//            y = newY;
-//        }
-//    }
-
-//    paintCanvas.addEventListener( 'mousedown', startDrawing );
-//    paintCanvas.addEventListener( 'mousemove', drawLine );
-//    paintCanvas.addEventListener( 'mouseup', stopDrawing );
-//    paintCanvas.addEventListener( 'mouseout', stopDrawing );
-
-//  return <canvas ref={canvasRef} {...rest}/>
-//}
-
 
 const Canvas = props => {
 
@@ -174,9 +108,6 @@ async function GetTeXResult(positions) {
 function ScryBlockView({ target }: EntityViewProps) {
 	const target_ir = target.get(ScryBlock.KEY);
 	const text = useListenable(target_ir.text);
-        //const [renderRef, draw] = useSvgDrawing({
-        //    penWidth: 5, // pen width
-        //})
         
         const canvas_ref = React.useRef(null);
 
@@ -187,104 +118,9 @@ function ScryBlockView({ target }: EntityViewProps) {
         }, []);
 
 
-        const PathToPoints = (path, resolution, onDone) => {
-            console.log("RUNNING")
-            var ctx = {};
-            ctx.resolution = resolution;
-            ctx.onDone = onDone;
-            ctx.points = [];
-            ctx.interval = null;
-
-
-            // Walk up nodes until we find the root svg node
-            var svg = path;
-
-
-            while (!(svg instanceof SVGSVGElement)) {
-                console.log(svg instanceof SVGSVGElement, "here")
-                svg = svg.parentElement;
-            }
-            // Create a rect, which will be used to trace the path
-
-            var rect = document.createElementNS("http://www.w3.org/2000/svg", "rect");
-            ctx.rect = rect;
-            svg.appendChild(rect);
-
-            var motion = document.createElementNS("http://www.w3.org/2000/svg", "animateMotion");
-            motion.setAttribute("path", path.getAttribute("d"));
-            motion.setAttribute("begin", "0");
-            motion.setAttribute("dur", "3"); // TODO: set this to some larger value, e.g. 10 seconds?
-            motion.setAttribute("repeatCount", "1");
-            motion.onbegin = PathToPoints.beginRecording.bind(this, ctx);
-            motion.onend = PathToPoints.stopRecording.bind(this, ctx);
-
-            // Add rect
-            rect.appendChild(motion);
-        }
-
-        PathToPoints.beginRecording = function(ctx) {
-            var m = ctx.rect.getScreenCTM();
-            ctx.points.push({x: m.e, y: m.f});
-            ctx.interval = setInterval(PathToPoints.recordPosition.bind(this, ctx), 1000*3/ctx.resolution);
-        }
-
-        PathToPoints.stopRecording = function(ctx) {
-            clearInterval(ctx.interval);
-
-            // Remove the rect
-            ctx.rect.remove();
-
-            ctx.onDone(ctx.points);
-        }
-
-        PathToPoints.recordPosition = function(ctx) {
-            var m = ctx.rect.getScreenCTM();
-            ctx.points.push({x: m.e, y: m.f});
-        }
-
-
-
-
-
-
-
-
-
 	return <div
             ref={block_ref}
 	>
-            {/*<div style={{ width: 300, height: 300 }} ref={renderRef}
-                onMouseEnter={(e) => {
-                    //console.log(e.clientX, e.clientY, "cliekd!")
-
-                    var rect = e.target.getBoundingClientRect();
-                    var x = e.clientX - rect.left; //x position within the element.
-                    var y = e.clientY - rect.top;  //y position within the element.
-                    console.log("Left? : " + x + " ; Top? : " + y + ".");
-                }}
-                />*/}
-            {/*<ReactSketchCanvas
-                width="300"
-                height="300"
-                strokeWidth={4}
-                strokeColor="black"
-            />*/}
-
-            {/*<Canvas 
-                draw={(ctx, frameCount) => {
-                    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height)
-                    ctx.fillStyle = '#000000'
-                    ctx.beginPath()
-                    ctx.arc(50, 100, 20*Math.sin(frameCount*0.05)**2, 0, 2*Math.PI)
-                    ctx.fill()
-                }}
-
-                options={{
-                    context: canvas_ref.current,
-                    onClick: (e) => { console.log(e) }
-                }}
-
-                />*/}
             <Canvas canvasRef={canvas_ref} />
 
             <div className="border-red-4 border-2"
@@ -292,38 +128,6 @@ function ScryBlockView({ target }: EntityViewProps) {
                     draw.clear()
                 }}
             > clear </div>
-            <div className="border-red-4 border-2"
-                onClick={() => { 
-                    let el = renderRef.current.children[0].children
-                    let paths = []
-                    //for (const child of el) {
-                    //    //console.log(child.parentElement, "whee")
-                    //    PathToPoints(child, 100, function(p){
-                    //        paths.append(p)
-                    //    })
-                    //}
-                    let cur_i = 0;
-
-                    //PathToPoints(el[cur_i], 10, function(p){
-                    //    console.log(p)
-                    //})
-
-                    //const recurse = (i) => {
-                    //    PathToPoints(el[cur_i], 100, function(p){
-                    //        console.log(p)
-                    //        paths.push(p)
-                    //        cur_i += 1
-                    //        if (el[cur_i] !== undefined) { 
-                    //            console.log("recursing!", cur_i, el[cur_i])
-                    //            recurse(cur_i) 
-                    //        }
-                    //    })
-                    //}
-
-                    //recurse(cur_i)
-
-                }}
-            > log </div>
 	</div>;
 }
 
