2109bfef2fa02a6e8a740d6afc52e43f47ea4bcd
zbuster05 <zakary.saya@gmail.com>
2022-12-06 17:39:51 -0600

    NW: potential changes

--- src/web/src/model/blocks/latex_scry.tsx
+++ src/web/src/model/blocks/latex_scry.tsx
@@ -119,8 +119,9 @@ export function createKind(parent: Part | null) {
 }
 
 async function GetTeXResult(positions) {
-    response = await fetch("https://detexify.kirelabs.org/api/classify", {
+    const response = await fetch("https://detexify.kirelabs.org/api/classify", {
     "headers": {
+    	"Origin": "https://detexify.kirelabs.org", 
 	"accept": "application/json, text/javascript, */*; q=0.01",
 	"accept-language": "en-US,en;q=0.9",
 	"content-type": "application/x-www-form-urlencoded; charset=UTF-8",
@@ -128,17 +129,18 @@ async function GetTeXResult(positions) {
 	"sec-ch-ua-mobile": "?0",
 	"sec-ch-ua-platform": "\"macOS\"",
 	"sec-fetch-dest": "empty",
-	"sec-fetch-mode": "cors",
-	//"sec-fetch-site": "same-origin",
+	"sec-fetch-mode": "no-cors",
+	"sec-fetch-site": "same-origin",
 	"x-requested-with": "XMLHttpRequest",
 	"cookie": "_ga=GA1.2.943797916.1670265125; _gid=GA1.2.845075545.1670265125; __gads=ID=36efcd96a0e29108-22ee6daa94d700c3:T=1670265125:RT=1670265125:S=ALNI_MZT8eT2UUPYHB66YidcWsSz7-WktA; __gpi=UID=0000090a20d605f4:T=1670265125:RT=1670265125:S=ALNI_MaWC54PqIZguojLuEvey4v4KnIdnQ",
 	"Referer": "https://detexify.kirelabs.org/classify.html",
-	//"Referrer-Policy": "strict-origin-when-cross-origin"
+	"Referrer-Policy": "strict-origin-when-cross-origin"
     },
+    "mode": "no-cors",
     "body": `strokes=${encodeURIComponent(JSON.stringify(positions))}`,
     "method": "POST"
     });
-    json = await response.json();
+    let json = await response.json();
     return json.filter(element => element.symbol.package != undefined).filter(element => element.symbol.mathmode).slice(0, 5).map(element => element.symbol.command);
 }
 
