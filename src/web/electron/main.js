459171aa1137ca4a5985b71baf8eb3f3bb840723
zbuster05 <zakary.saya@gmail.com>
2022-12-07 19:20:46 -0600

    Basic working electron with inset lights and scry

---
+++ src/web/electron/main.js
@@ -0,0 +1,200 @@
+const { app, BrowserWindow, systemPreferences, nativeTheme, ipcMain, Menu, shell } = require('electron');
+const { autoUpdater } = require("electron-updater");
+autoUpdater.checkForUpdatesAndNotify();
+const path = require('path');
+const isDev = require('electron-is-dev');
+
+
+function createWindow () {
+    // Create the browser window.
+    let win = new BrowserWindow({
+        'width': 1000,
+        'height': 650,
+        'minWidth': 400,
+        'minHeight': 650,
+	'transparent': true,
+        'title': "Kotae",
+        'webPreferences': {
+            'webSecurity': false,
+            'nodeIntegration': true
+        },
+        'titleBarStyle': 'hiddenInset',
+        'transparent': true,
+        'show': false,
+    });
+// TODO fix
+    if(nativeTheme.shouldUseDarkColors) {
+        win.setBackgroundColor("#161616");
+    } else {
+        win.setBackgroundColor("#f4f4f4");
+    }
+
+    win.on('page-title-updated', function(e) {
+        e.preventDefault()
+    });
+
+    // TODO TODO TODO UNCOMMENT THIS!!
+    win.removeMenu();
+    // and load the main of the app.
+    if (isDev)
+        win.loadURL(`http://localhost:1234`);
+    else
+        win.loadURL(`file://${path.join(__dirname, './src/index.html')}`);
+   
+	win.webContents.setWindowOpenHandler(({ url }) => {
+        // config.fileProtocol is my custom file protocol
+        if (url.startsWith("http") || url.startsWith("https")) {
+            shell.openExternal(url);
+            return { action: 'deny' };
+        }
+        return { action: 'allow' };
+    });
+
+    win.once('ready-to-show', function() {
+        win.show()
+    });
+}
+/*
+function createAbout () {
+    // Create the browser window.
+    let win = new BrowserWindow({
+        'width': 550,
+        'height': 175,
+        'minWidth': 550,
+        'minHeight': 175,
+        'maxWidth': 550,
+        'maxHeight': 175,
+        'title': "About Condution",
+        'webPreferences': {
+            'nodeIntegration': true,
+            'contextIsolation': false
+
+        },
+        'maximizable': false,
+        //'frame': false,
+        'show': false,
+    });
+
+    win.on('page-title-updated', function(e) {
+        e.preventDefault()
+    });
+
+    win.removeMenu();
+    // and load the main of the app.
+    win.loadFile('src/about.html');
+    
+    win.once('ready-to-show', function() {
+        win.show()
+    });
+}
+*/
+const isMac = process.platform === 'darwin';
+
+const template = [
+  // { role: 'appMenu' }
+  ...(isMac ? [{
+    label: app.name,
+    submenu: [
+      /*{
+        label: 'About Condution',
+        click: async () => {
+            createAbout();
+        }
+      },*/
+      { type: 'separator' },
+      { role: 'services' },
+      { type: 'separator' },
+      { role: 'hide' },
+      { role: 'hideothers' },
+      { role: 'unhide' },
+      { type: 'separator' },
+      { role: 'quit' }
+    ]
+  }] : []),
+  // { role: 'fileMenu' }
+  {
+    label: 'File',
+    submenu: [
+      isMac ? { role: 'close' } : { role: 'quit' }
+    ]
+  },
+  // { role: 'editMenu' }
+  {
+    label: 'Edit',
+    submenu: [
+      { role: 'undo' },
+      { role: 'redo' },
+      { type: 'separator' },
+      { role: 'cut' },
+      { role: 'copy' },
+      { role: 'paste' },
+      ...(isMac ? [
+        { role: 'pasteAndMatchStyle' },
+        { role: 'delete' },
+        { role: 'selectAll' },
+        { type: 'separator' },
+        {
+          label: 'Speech',
+          submenu: [
+            { role: 'startspeaking' },
+            { role: 'stopspeaking' }
+          ]
+        }
+      ] : [
+        { role: 'delete' },
+        { type: 'separator' },
+        { role: 'selectAll' }
+      ])
+    ]
+  },
+  // { role: 'viewMenu' }
+  {
+    label: 'View',
+    submenu: [
+      { role: 'reload' },
+      { role: 'forcereload' },
+      { role: 'toggledevtools' },
+      { type: 'separator' },
+      { role: 'resetzoom' },
+      { role: 'zoomin' },
+      { role: 'zoomout' },
+      { type: 'separator' },
+      { role: 'togglefullscreen' }
+    ]
+  },
+  // { role: 'windowMenu' }
+  {
+    label: 'Window',
+    submenu: [
+      { role: 'minimize' },
+      { role: 'zoom' },
+      ...(isMac ? [
+        { type: 'separator' },
+        { role: 'front' },
+        { type: 'separator' },
+        { role: 'window' }
+      ] : [
+        { role: 'close' }
+      ])
+    ]
+  },
+  {
+    role: 'help',
+    submenu: [
+      {
+        label: 'Help and Documentation',
+        click: async () => {
+          const { shell } = require('electron');
+          await shell.openExternal('https://huxleymarvit.com')
+        }
+      }
+    ]
+  }
+];
+
+const menu = Menu.buildFromTemplate(template);
+Menu.setApplicationMenu(menu);
+
+app.name = 'kotae';
+app.whenReady().then(createWindow);
+
