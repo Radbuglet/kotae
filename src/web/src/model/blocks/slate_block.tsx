56cf4edd986f4dc83b89a1de90980cf86577074f
Nicholas Assaderaghi <nicassa@nuevaschool.org>
2022-12-10 15:37:35 -0800

    working on slate block.

--- src/web/src/model/blocks/slate_block.tsx
+++ src/web/src/model/blocks/slate_block.tsx
@@ -1,9 +1,20 @@
-import React from "react";
+import React, { useMemo, useRef, useEffect } from 'react'
 
 import { Entity, Part } from "kotae-util";
-import { EntityViewProps } from "../../util/hooks";
+import { EntityViewProps, useListenable } from "../../util/hooks";
 import { BLOCK_FACTORY_KEY, BLOCK_KIND_INFO_KEY, BLOCK_VIEW_KEY } from "../registry";
 import { IrBlock, SlateBlock } from "kotae-common";
+import { Slate, Editable, withReact, useSlate, useFocused } from 'slate-react'
+import {
+  Editor,
+  Transforms,
+  Text,
+  createEditor,
+  Descendant,
+  Range,
+} from 'slate'
+import { css } from '@emotion/css'
+import { withHistory } from 'slate-history'
 
 export function createKind(parent: Part | null): Entity {
     const kind = new Entity(parent, "slate block kind");
@@ -33,5 +44,42 @@ export function createKind(parent: Part | null): Entity {
 }
 
 export function SlateBlockView({ target }: EntityViewProps) {
-    return <h1>Hello, I am a slate block.</h1>;
+    const target_ir = target.get(SlateBlock.KEY);
+	const text = useListenable(target_ir.stringified_json);
+
+	const block_ref = React.useRef<HTMLDivElement>(null);
+
+	React.useEffect(() => {
+		block_ref.current!.focus();
+	}, []);
+
+    return <div
+		className="outline-none"
+		ref={block_ref}
+		contentEditable={true}
+		onBlur={(e) => {
+			//target_ir.stringified_json.value = 
+		}}
+	>
+		<Slate editor={editor} value={initialValue}>
+            <HoveringToolbar />
+            <Editable
+                renderLeaf={props => <Leaf {...props} />}
+                placeholder="Enter some text..."
+                onDOMBeforeInput={(event: InputEvent) => {
+                switch (event.inputType) {
+                    case 'formatBold':
+                    event.preventDefault()
+                    return toggleFormat(editor, 'bold')
+                    case 'formatItalic':
+                    event.preventDefault()
+                    return toggleFormat(editor, 'italic')
+                    case 'formatUnderline':
+                    event.preventDefault()
+                    return toggleFormat(editor, 'underlined')
+                }
+                }}
+            />
+        </Slate>
+	</div>;
 }
