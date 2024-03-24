792dcd5c70589ada453c82c7aaf84985fb386b0f
Huxley Marvit <huxley@princeton.edu>
2024-03-24 17:08:40 -0400

    make slate block properly focus on init and swap out to new markdown converter

--- src/web/src/model/blocks/slate_block.tsx
+++ src/web/src/model/blocks/slate_block.tsx
@@ -17,7 +17,9 @@ import { withHistory } from 'slate-history'
 import { css } from '@emotion/css'
 import Prism from 'prismjs'
 import escapeHtml from 'escape-html'
-import markdown, { getCodeString } from '@wcj/markdown-to-html';
+
+//import markdown, { getCodeString } from '@wcj/markdown-to-html';
+
 import { convertText } from './slate_block_utils/html_to_latex' // this errors because it's written in JS, not an actual issue though when we run the app
 
 // this is a markdown rich text editor!
@@ -86,6 +88,9 @@ const initialValue: Descendant[] = [
 // Modified from this reference: https://docs.slatejs.org/concepts/10-serializing
 /** Recursively serializes the slate block from a markdown string to a html string. */
 const serialize = node => {
+    let showdown  = require('showdown');
+    let converter = new showdown.Converter()
+
     if (Text.isText(node)) {
         let string = escapeHtml(node.text) // escape any html reserved characters, because we're exporting to html
         if (string.startsWith("&gt")) {
@@ -98,7 +103,9 @@ const serialize = node => {
     }
 
     const children = node.children.map(n => serialize(n)).join('') // serialize recursively, the data of the slate block is of a tree structure
-    return markdown(children) as string // the markdown function converts markdown strings to html
+    //return markdown(children) as string // the markdown function converts markdown strings to html
+    //return children as string // the markdown function converts markdown strings to html
+    return converter.makeHtml(children) as string // the markdown function converts markdown strings to html
 }
 
 // ! starting here is regular kotae code.
@@ -134,14 +141,19 @@ export function SlateBlockView({ target }: EntityViewProps) {
     //console.log(text) // debug
 
     const block_ref = React.useRef<HTMLDivElement>(null);
+    const editable = React.useRef(null);
 
-    React.useEffect(() => {
-        block_ref.current!.focus();
-    }, []);
 
     // this is all from https://github.com/ianstormtaylor/slate/blob/main/site/examples/markdown-preview.tsx#L6
     const renderLeaf = useCallback(props => <Leaf {...props} />, [])
     const editor = useMemo(() => withHistory(withReact(createEditor())), [])
+
+    React.useEffect(() => {
+        //block_ref.current!.focus(); // this is not working
+        // focus the editor element
+        editor.selection = { anchor: { path: [0, 0], offset: 0 }, focus: { path: [0, 0], offset: 0 } }
+    }, []);
+
     const decorate = useCallback(([node, path]) => {
         const ranges = []
 
@@ -192,11 +204,17 @@ export function SlateBlockView({ target }: EntityViewProps) {
         }}
         onKeyDown={handleKeydown}
     >
-        <Slate editor={editor} value={initialValue}>
+        <Slate editor={editor}
+            value={initialValue}
+            initialValue={initialValue}
+        >
             <Editable
+                ref={editable}
                 decorate={decorate}
                 renderLeaf={renderLeaf}
                 placeholder=""
+                autoFocus={true}
+                className="outline-none"
             />
         </Slate>
     </div>;
